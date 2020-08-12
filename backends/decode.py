f49ccd03b051cfb24113ef7cde0e5a3311d5bc8f
Tobias Scheck <tobias.scheck@etit.tu-chemnitz.de>
2020-08-12 13:06:06 +0200

    fix int division for pytorch 1.6

--- backends/decode.py
+++ backends/decode.py
@@ -18,11 +18,11 @@ def _topk(scores, K=40):
     topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
 
     topk_inds = topk_inds % (height * width)
-    topk_ys = (topk_inds / width).int().float()
+    topk_ys = (topk_inds // width).int().float()
     topk_xs = (topk_inds % width).int().float()
 
     topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
-    topk_clses = (topk_ind / K).int()
+    topk_clses = (topk_ind // K).int()
     topk_inds = _gather_feat(
         topk_inds.view(batch, -1, 1), topk_ind).view(batch, K)
     topk_ys = _gather_feat(topk_ys.view(batch, -1, 1), topk_ind).view(batch, K)
