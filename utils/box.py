57c88aa9a2e19e3f5ebe12aeb9a375c7193e5ece
Tobias Scheck <tobias.scheck@etit.tu-chemnitz.de>
2020-10-08 09:05:15 +0200

    refactored

--- utils/box.py
+++ utils/box.py
@@ -1,8 +1,7 @@
 import numpy as np
 
+
 def get_annotation_with_angle(ann):
-    #print(ann['bbox'])
-    img_id = ann['image_id']
     new_ann = None
     # get width and height
     if not 'rbbox' in ann:
@@ -12,30 +11,38 @@ def get_annotation_with_angle(ann):
         bbox[1] = bbox[1] + bbox[3] / 2
         bbox.append(0)
         if bbox[2] > bbox[3]:
-            bbox[2],ann['bbox'][3] = bbox[3],bbox[2]
+            bbox[2], ann['bbox'][3] = bbox[3], bbox[2]
             bbox[4] -= 90
         new_ann = bbox
     else:
         # using rotated bounding box datasets. 5 = [cx,cy,w,h,angle]
-        assert len(ann['rbbox']) == 5, 'Unknown bbox format' # x,y,w,h,a
+        assert len(ann['rbbox']) == 5, 'Unknown bbox format'  # x,y,w,h,a
         new_ann = np.array(ann['rbbox'], dtype=np.float32)
         if new_ann[2] > new_ann[3]:
-            new_ann[2], new_ann[3] = new_ann[3],new_ann[2]
+            new_ann[2], new_ann[3] = new_ann[3], new_ann[2]
             new_ann[4] += (90.0 - 0.001)
 
-    if new_ann[2] == new_ann[3]:
-        new_ann[3] += 1 # force that w < h
+        new_ann[4] = np.clip(new_ann[4], -90, 90 - 0.001)
 
-    assert new_ann[2] < new_ann[3]
-    assert new_ann[4] >= -90 and new_ann[4] < 90
+    if new_ann[2] == new_ann[3]:
+        new_ann[3] += 1  # force that w < h
+
+    assert new_ann[2] < new_ann[3], "width not smaller than height"
+    assert (new_ann[4] >= -90 and
+            new_ann[4] < 90), f"{new_ann[4]} not in interval [-90, 90)"
 
     # override original bounding box with rotated bounding box
     return new_ann
 
-def rotate_bbox(x,y,w,h,angle):
-    c, s = np.cos(angle/180*np.pi), np.sin(angle/180*np.pi)
+
+def rotate_bbox(x, y, w, h, angle):
+    c, s = np.cos(angle / 180 * np.pi), np.sin(angle / 180 * np.pi)
     R = np.asarray([[c, s], [-s, c]])
-    pts = np.asarray([[-w/2, -h/2], [w/2, -h/2], [w/2, h/2], [-w/2, h/2]])
+    pts = np.asarray(
+        [[-w / 2, -h / 2],
+         [w / 2, -h / 2],
+         [w / 2, h / 2],
+         [-w / 2, h / 2]])
     rot_pts = []
     for pt in pts:
         rot_pts.append(([x, y] + pt @ R).astype(int))
