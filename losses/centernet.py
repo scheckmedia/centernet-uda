32bef07888fdd92a64ff92155c9452f7f75cb1f6
Tobias Scheck <tobias.scheck@etit.tu-chemnitz.de>
2020-10-24 10:19:47 +0200

    fix weights for angle

--- losses/centernet.py
+++ losses/centernet.py
@@ -5,15 +5,16 @@ import numpy as np
 
 
 class DetectionLoss(torch.nn.Module):
-    def __init__(self, hm_weight, wh_weight, off_weight, periodic=False):
+    def __init__(self, hm_weight, wh_weight, off_weight,
+                 angle_weight=1.0, periodic=False):
         super().__init__()
-        self.crit_hm = FocalLoss()
-        self.crit_reg = RegL1Loss()
-        self.crit_hw = RegL1Loss() if not periodic else PeriodicRegL1Loss()
-
-        self.hm_weight = hm_weight
-        self.wh_weight = wh_weight
-        self.off_weight = off_weight
+        self.crit_hm = FocalLoss(weight=hm_weight)
+        self.crit_reg = RegL1Loss(off_weight)
+        self.crit_hw = RegL1Loss(
+            wh_weight,
+            angle_weight) if not periodic else PeriodicRegL1Loss(
+            wh_weight,
+            angle_weight)
 
     def forward(self, output, batch):
         hm_loss, wh_loss, off_loss = 0.0, 0.0, 0.0
@@ -26,9 +27,7 @@ class DetectionLoss(torch.nn.Module):
         off_loss += self.crit_reg(output['reg'], batch['reg_mask'],
                                   batch['ind'], batch['reg'])
 
-        loss = self.hm_weight * hm_loss
-        loss += self.wh_weight * wh_loss
-        loss += self.off_weight * off_loss
+        loss = hm_loss + wh_loss + off_loss
 
         loss_stats = {'centernet_loss': loss, 'hm_loss': hm_loss,
                       'wh_loss': wh_loss, 'off_loss': off_loss}
@@ -38,8 +37,9 @@ class DetectionLoss(torch.nn.Module):
 class FocalLoss(torch.nn.Module):
     '''nn.Module warpper for focal loss'''
 
-    def __init__(self):
+    def __init__(self, weight=1.0):
         super().__init__()
+        self.weight = weight
 
     def forward(self, out, target):
         return self._neg_loss(out, target)
@@ -70,25 +70,52 @@ class FocalLoss(torch.nn.Module):
             loss = loss - neg_loss
         else:
             loss = loss - (pos_loss + neg_loss) / num_pos
-        return loss
+        return loss * self.weight
 
 
 class RegL1Loss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, weight=1.0, angle_weight=1.0):
         super().__init__()
+        self.weight = weight
+        self.angle_weight = angle_weight
 
     def forward(self, output, mask, ind, target):
         pred = _transpose_and_gather_feat(output, ind)
         mask = mask.unsqueeze(2).expand_as(pred).float()
-        # loss = F.l1_loss(pred * mask, target * mask, reduction='elementwise_mean')
-        loss = F.l1_loss(pred * mask, target * mask, size_average=False)
-        loss = loss / (mask.sum() + 1e-4)
+
+        pred *= mask
+        target *= mask
+
+        # if we have angle
+        if pred.shape[-1] == 3:
+            pred_wh = pred[..., 0:2]
+            pred_angle = pred[..., 2:3]
+
+            target_wh = target[..., 0:2]
+            target_angle = target[..., 2:3]
+
+            loss = F.l1_loss(pred_wh, target_wh, size_average=False)
+            loss = loss / (mask.sum() + 1e-4)
+
+            a_loss = F.l1_loss(pred_angle, target_angle, size_average=False)
+            a_loss = a_loss / (mask.sum() + 1e-4)
+
+            loss *= self.weight
+            loss += a_loss * self.angle_weight
+
+        else:
+            loss = F.l1_loss(pred, target, size_average=False)
+            loss = loss / (mask.sum() + 1e-4)
+            loss *= self.weight
+
         return loss
 
 
 class PeriodicRegL1Loss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, wh_weight=1.0, angle_weight=1.0):
         super().__init__()
+        self.wh_weight = wh_weight
+        self.angle_weight = angle_weight
 
     def forward(self, output, mask, ind, target):
         pred = _transpose_and_gather_feat(output, ind)
@@ -112,5 +139,6 @@ class PeriodicRegL1Loss(torch.nn.Module):
         )
         periodic_loss = periodic_loss.sum() / (mask.sum() + 1e-4)
 
-        loss += periodic_loss
+        loss *= self.wh_weight
+        loss += periodic_loss * self.angle_weight
         return loss
