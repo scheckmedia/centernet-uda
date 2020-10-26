25ffc4fab07204c34fac9c7dcf587be7d02bf016
Tobias Scheck <tobias.scheck@etit.tu-chemnitz.de>
2020-10-26 14:49:44 +0100

    add test phase

--- train.py
+++ train.py
@@ -48,7 +48,22 @@ def load_datasets(cfg, down_ratio, rotated_boxes):
 
     log.info(f"Found {len(training)} samples in training dataset")
 
-    return training_loader, validation_loader
+    test_loader = None
+    if 'test' in cfg.datasets:
+        test = hydra.utils.get_class(
+            f'datasets.{cfg.datasets.test.name}.Dataset')
+        params = {**cfg.datasets.test.params, **defaults}
+        test = test(**params)
+        test_loader = DataLoader(
+            test,
+            batch_size=cfg.batch_size,
+            shuffle=False,
+            num_workers=cfg.num_workers,
+            pin_memory=True)
+
+        log.info(f"Found {len(test)} samples in test dataset")
+
+    return training_loader, validation_loader, test_loader
 
 
 @hydra.main(config_path="configs/defaults.yaml")
@@ -91,7 +106,7 @@ def main(cfg: DictConfig) -> None:
 
     uda.scheduler = scheduler
 
-    train_loader, val_loader = load_datasets(
+    train_loader, val_loader, test_loader = load_datasets(
         cfg, down_ratio=backend.down_ratio, rotated_boxes=backend.rotated_boxes)
     tensorboard_logger = TensorboardLogger(cfg, val_loader.dataset.classes)
 
@@ -112,38 +127,101 @@ def main(cfg: DictConfig) -> None:
     stats = {}
     best = 1e10 if cfg.save_best_metric.mode == 'min' else 1e-10
 
-    for epoch in tqdm(
-            range(start_epoch, cfg.epochs + 1),
-            initial=start_epoch,
-            position=0, desc='Epoch'):
-        uda.epoch_start()
-        uda.backend.train()
-        tag = 'training'
-        for step, data in tqdm(
-                enumerate(train_loader),
-                total=len(train_loader),
-                position=1, desc='Training Steps'):
-            outputs = uda.step(data)
-
-            for k in outputs["stats"]:
-                log_key = f"{tag}/{k}"
-                m = stats.get(log_key, AverageMeter(name=k))
-                m.update(outputs["stats"][k].item(), data["input"].size(0))
-                stats[log_key] = m
-
-        tag = 'validation'
-        uda.backend.eval()
-        with torch.no_grad():
+    if not cfg.test_only:
+        for epoch in tqdm(
+                range(start_epoch, cfg.epochs + 1),
+                initial=start_epoch,
+                position=0, desc='Epoch'):
+            uda.epoch_start()
+            uda.backend.train()
+            tag = 'training'
             for step, data in tqdm(
-                    enumerate(val_loader),
-                    total=len(val_loader),
-                    position=1, desc='Validation Steps'):
-                outputs = uda.step(data, is_training=False)
+                    enumerate(train_loader),
+                    total=len(train_loader),
+                    position=1, desc='Training Steps'):
+                outputs = uda.step(data)
 
                 for k in outputs["stats"]:
                     log_key = f"{tag}/{k}"
                     m = stats.get(log_key, AverageMeter(name=k))
                     m.update(outputs["stats"][k].item(), data["input"].size(0))
+                    stats[log_key] = m
+
+            tag = 'validation'
+            uda.backend.eval()
+            with torch.no_grad():
+                for step, data in tqdm(
+                        enumerate(val_loader),
+                        total=len(val_loader),
+                        position=1, desc='Validation Steps'):
+                    outputs = uda.step(data, is_training=False)
+
+                    for k in outputs["stats"]:
+                        log_key = f"{tag}/{k}"
+                        m = stats.get(log_key, AverageMeter(name=k))
+                        m.update(
+                            outputs["stats"][k].item(),
+                            data["input"].size(0))
+                        stats[log_key] = m
+
+                    detections = uda.get_detections(outputs, data)
+                    detections["image_shape"] = data["input"].shape[1:]
+                    for e in evaluators:
+                        e.add_batch(**detections)
+
+                    tensorboard_logger.log_detections(
+                        data, detections, epoch, tag=tag)
+
+            for e in evaluators:
+                result = e.evaluate()
+                stats = {**stats, **result}
+
+            scalars = {}
+            for k, s in stats.items():
+                if isinstance(s, AverageMeter):
+                    scalars[k] = s.avg
+                    s.reset()
+                else:
+                    scalars[k] = s
+
+                tensorboard_logger.log_stat(k, scalars[k], epoch)
+
+            uda.epoch_end()
+            tensorboard_logger.reset()
+            uda.save_model("model_last.pth", epoch, True)
+
+            if not cfg.save_best_metric.name in scalars:
+                log.error(
+                    f"Metric {cfg.save_best_metric.name} not valid, valid values are {' '.join(scalars.keys())}")
+                return
+
+            current = scalars[cfg.save_best_metric.name]
+            if (cfg.save_best_metric.mode == 'min' and best > current
+                    or cfg.save_best_metric.mode == 'max' and best < current):
+                uda.save_model("model_best.pth", epoch, True)
+                best = current
+
+                log.info(
+                    f"Save best model with {cfg.save_best_metric.name} of {current:.4f}")
+
+    if test_loader is not None:
+        if not cfg.test_only:
+            epoch = start_epoch
+        tag = 'test'
+        uda.backend.eval()
+        with torch.no_grad():
+            for step, data in tqdm(
+                    enumerate(test_loader),
+                    total=len(test_loader),
+                    position=1, desc='Test Steps'):
+                outputs = uda.step(data, is_training=False)
+
+                for k in outputs["stats"]:
+                    log_key = f"{tag}/{k}"
+                    m = stats.get(log_key, AverageMeter(name=k))
+                    m.update(
+                        outputs["stats"][k].item(),
+                        data["input"].size(0))
                     stats[log_key] = m
 
                 detections = uda.get_detections(outputs, data)
@@ -152,7 +230,7 @@ def main(cfg: DictConfig) -> None:
                     e.add_batch(**detections)
 
                 tensorboard_logger.log_detections(
-                    data, detections, epoch, tag='validation')
+                    data, detections, epoch, tag=tag)
 
         for e in evaluators:
             result = e.evaluate()
@@ -168,23 +246,7 @@ def main(cfg: DictConfig) -> None:
 
             tensorboard_logger.log_stat(k, scalars[k], epoch)
 
-        uda.epoch_end()
         tensorboard_logger.reset()
-        uda.save_model("model_last.pth", epoch, True)
-
-        if not cfg.save_best_metric.name in scalars:
-            log.error(
-                f"Metric {cfg.save_best_metric.name} not valid, valid values are {' '.join(scalars.keys())}")
-            return
-
-        current = scalars[cfg.save_best_metric.name]
-        if (cfg.save_best_metric.mode == 'min' and best > current
-                or cfg.save_best_metric.mode == 'max' and best < current):
-            uda.save_model("model_best.pth", epoch, True)
-            best = current
-
-            log.info(
-                f"Save best model with {cfg.save_best_metric.name} of {current:.4f}")
 
 
 if __name__ == "__main__":
